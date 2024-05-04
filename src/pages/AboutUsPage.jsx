cd6a7a92b18e8562e53efa0e9ce921b56f0a6ee2
Tanmay Roy <tanmayroy84368@gmail.com>
2024-05-04 22:25:45 +0530

    Add: about us page responsive design

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,5 +1,4 @@
 import { COLORS } from "../assets/constants";
-import Banner from "../components/banner/Banner";
 import CallToAction from "../components/callToAction/CallToAction";
 import CoreValues from "../components/coreValues/CoreValues";
 import FindUs from "../components/findUs/FindUs";
@@ -10,6 +9,7 @@ import Places from "../components/places/Places";
 import Vision from "../components/vision/Vision";
 import styles from "./AboutUsPage.module.css";
 import { CoreValuesData } from "../assets/coreValuesData";
+import Button from "../components/button/Button";
 
 const AboutUsPage = () => {
   return (
@@ -20,17 +20,39 @@ const AboutUsPage = () => {
         iconColor={COLORS.black}
       />
 
-      <Banner
+      {/* <Banner
         imagePath={"/images/about_us_hero.png"}
         heading="The Consulting<br />that Gets you more"
         para="We at ChemBizR, partner with global<br />clients to unleash unique solutions across<br />a variety of business verticals."
         buttonText="Insights"
         textColor={COLORS.black}
-        headingFontSize="56px"
-        headingFontWeight="600"
         headingMarginTop="120px"
         headingLineHeight="135%"
-      />
+      /> */}
+
+      <div className={styles.bannerContainer}>
+        <div className={styles.bannerContent}>
+          <h1
+            className={styles.bannerHeading}
+            dangerouslySetInnerHTML={{
+              __html: "The Consulting<br />that Gets you more",
+            }}
+          />
+          <p
+            className={styles.bannerPara}
+            dangerouslySetInnerHTML={{
+              __html:
+                "We at ChemBizR, partner with global<br />clients to unleash unique solutions across<br />a variety of business verticals.",
+            }}
+          />
+
+          <Button content="Insights" bgColor={COLORS.orange} />
+        </div>
+
+        <div className={styles.bannerImage}>
+          <img src="/images/about_us_hero.png" alt="image" />
+        </div>
+      </div>
 
       <OriginStory />
 
