9bc38887fa84ccd1c7dd3275dd6bc9a99e0e0efc
Tanmay Roy <tanmayroy84368@gmail.com>
2024-05-05 23:43:47 +0530

    Add: focus page responsive design

--- src/pages/FocusParentPage.jsx
+++ src/pages/FocusParentPage.jsx
@@ -44,7 +44,7 @@ const FocusParentPage = () => {
       <Banner
         imagePath={"/images/focus_parent_page_hero.png"}
         heading="Personal Care<br />Magazine: Making<br />sense of the anti-<br />pollution segment"
-        para="Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br />Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        para="Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
         buttonText="Read More"
         headingLineHeight="120%"
       />
@@ -68,7 +68,7 @@ const FocusParentPage = () => {
       <CareAndCosmetics />
 
       {/* More Focus Areas */}
-      <div className={styles.carouselContainer}>
+      {/* <div className={styles.carouselContainer}>
         <h3 className={styles.heading}>More of Our Focus Verticals</h3>
         <h5 className={styles.subHeading}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -84,7 +84,7 @@ const FocusParentPage = () => {
             />
           ))}
         </Carousel>
-      </div>
+      </div> */}
 
       {/* Call to Action */}
       <CallToAction />
