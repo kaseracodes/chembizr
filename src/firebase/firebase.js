abdc7b2be36ca5da7fe05c814ff1ec64850aa637
Chikzz2001 <dibyabrata1234@gmail.com>
2024-06-05 17:13:00 +0530

    exposed credentials

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,25 +9,25 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
-
 // const firebaseConfig = {
-//     apiKey: "AIzaSyB0TLAKOU6tC08D2ZoIuBLzCe3AwRiQRuM",
-//     authDomain: "chembizr-5eabc.firebaseapp.com",
-//     projectId: "chembizr-5eabc",
-//     storageBucket: "chembizr-5eabc.appspot.com",
-//     messagingSenderId: "622136710677",
-//     appId: "1:622136710677:web:2343813a1c093af041a01c",
-//     measurementId: "G-H51W1RN3S7"
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 // };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPHFrCvy5OyQFxf_Cul6-529NuWkOyBfs",
+    authDomain: "chembizr-13d5a.firebaseapp.com",
+    projectId: "chembizr-13d5a",
+    storageBucket: "chembizr-13d5a.appspot.com",
+    messagingSenderId: "617645771031",
+    appId: "1:617645771031:web:36215842114a7cecddcf52",
+    measurementId: "G-SB711LNF6L"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
