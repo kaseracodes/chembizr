d5b4073f90a95d3246c7756f0fe23e1bfcbe08f3
Tanmay Roy <tanmayroy84368@gmail.com>
2024-06-13 13:34:21 +0530

    Fix: few changes in news page

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -7,7 +7,7 @@ import { COLORS } from "../assets/constants";
 import NewsListingCard from "../components/newsListingCard/NewsListingCard";
 import CallToAction from "../components/callToAction/CallToAction";
 import Footer from "../components/footer/Footer";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { firestore } from "../firebase/firebase";
 import {
   collection,
@@ -17,23 +17,10 @@ import {
   where,
 } from "firebase/firestore";
 import Pagination from "../components/pagination/Pagination";
-import { useSearchParams } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 const NewsPage = () => {
   const BussinessVerticalsItems = [
-    // "Adhesives and Sealants",
-    // "Animal Feed and Nutrition",
-    // "Base Oils and Waxes",
-    // "Bio Polymers",
-    // "Composites",
-    // "Construction",
-    // "Energy and Resources",
-    // "Food and Nutrition",
-    // "Industrial Fluids",
-    // "Mobility",
-    // "Oil and Gas",
-    // "Paints and Coatings",
-    // "Personal Care",
     "All",
     "Adhesives and Sealants",
     "Animal Feed and Nutrition",
@@ -83,18 +70,25 @@ const NewsPage = () => {
     }
 
     const unsubscribe = onSnapshot(q, (snapshot) => {
-        setNewsData(snapshot.docs);
-        console.log(snapshot.docs);
-      }
-    );
+      setNewsData(snapshot.docs);
+      console.log(snapshot.docs);
+    });
 
     return unsubscribe;
   }, [activeButton]);
 
+  const newsCardDivRef = useRef(null);
+  const { pathname } = useLocation();
+  const currentPage = new URLSearchParams(window.location.search).get("page");
+
+  useEffect(() => {
+    newsCardDivRef.current.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname, currentPage]);
+
   const [searchParams] = useSearchParams();
   const page = searchParams.get("page") || 1;
 
-  const NEWS_PER_PAGE = 3;
+  const NEWS_PER_PAGE = 4;
   const hasPrev = NEWS_PER_PAGE * (page - 1) > 0;
   const hasNext = NEWS_PER_PAGE * page < newsData.length;
   const startIndex = NEWS_PER_PAGE * (page - 1);
@@ -111,7 +105,7 @@ const NewsPage = () => {
       <Banner2 imagePath="/images/news_page_hero.png" heading="Industry News" />
 
       <div className={styles.newsListingDiv}>
-        <div className={styles.newsCardDiv}>
+        <div className={styles.newsCardDiv} ref={newsCardDivRef}>
           {newsData.slice(startIndex, endIndex).map((item, index) => (
             <NewsListingCard
               key={index}
